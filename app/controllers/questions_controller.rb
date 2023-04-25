104e5645711ac1ce5abdcc165708adb7bd0bcabb
aurora muggianu <auroraemu@gmail.com>
2023-04-25 21:57:01 +0200

    stupid coaching on rails done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @doubt = params[:doubt]
+    if @doubt == 'I am going to work'
+      @answer = 'Great'
+    elsif @doubt.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
